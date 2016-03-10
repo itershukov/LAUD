275b6d15a01b67ab34b333c91e8f1770946cfcc0
Ivan Tershukov <itersh@it>
2016-03-10 17:12:56 +0300

    add urls

--- app/src/main/java/com/example/laud/laud/ScrollingActivity.java
+++ app/src/main/java/com/example/laud/laud/ScrollingActivity.java
@@ -33,14 +33,26 @@ public class ScrollingActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 Log.i("Click to action","Try play mus");
-                String url = "http://streams.lrn.fm:8100/";//"rtsp://192.168.110.49:8554/5";
+//                String url = "http://streams.lrn.fm:8100/";//"rtsp://192.168.110.49:8554/5";
+                String url = "rtsp://192.168.110.49:8554/test.mp3";
+//                String url = "http://192.168.110.49:8554/test.mp3";
 //                String url = "rtsp://192.168.110.49:8554/5";
                 MediaPlayer mp = new MediaPlayer();
                 try {
                     mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
                     mp.setDataSource(getApplicationContext(), Uri.parse(url));
-                    mp.prepare();
-                    mp.start();
+
+                    mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mp.start();
+
+                        }
+                    });
+
+//                    mp.prepare();
+//                    mp.start();
                 } catch (Exception e) {
                     Log.i("Exception", "Exception in streaming mediaplayer e = " + e);
                     e.printStackTrace();
