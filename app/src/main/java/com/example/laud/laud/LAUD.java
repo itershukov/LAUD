e0070039d1f395f290fdff531c8cd89cce4cc4a1
Sinires <sinires@gmail.com>
2016-03-11 18:38:48 +0300

    Merge branch 'master' of github.com:itershukov/LAUD
    Conflicts:
    	.gradle/2.8/taskArtifacts/cache.properties.lock
    	.gradle/2.8/taskArtifacts/fileHashes.bin
    	.gradle/2.8/taskArtifacts/fileSnapshots.bin
    	.gradle/2.8/taskArtifacts/outputFileStates.bin
    	.gradle/2.8/taskArtifacts/taskArtifacts.bin
    	app/src/main/java/com/example/laud/laud/ScrollingActivity.java

--- app/src/main/java/com/example/laud/laud/LAUD.java
+++ app/src/main/java/com/example/laud/laud/LAUD.java
@@ -2,12 +2,13 @@ package com.example.laud.laud;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import java.io.IOException;
 
 public class LAUD extends AppCompatActivity {
-
+    String TAG = "LAUD";
     MediaControl md;
     Stream baseStream;
     @Override
@@ -16,18 +17,22 @@ public class LAUD extends AppCompatActivity {
         setContentView(R.layout.activity_laud);
         md = new MediaControl();
         Stream one = new Stream("http://streams.lrn.fm:8100/", "Стрим 1", "ссылка на картинку");
-        baseStream = one;
+        Stream two = new Stream("rtsp://195.209.248.200:554/tvorigin/33", "Стрим 2", "ссылка на картинку");
+        baseStream = two;
     }
 
     public void startStream(View view) {
+        Log.d(TAG, "Try start stream");
         try {
             md.playStream(baseStream);
         } catch (IOException e) {
             e.printStackTrace();
+            Log.d(TAG, "stream start problem");
         }
     }
 
     public void stopStream(View view) {
+        Log.d(TAG, "try stop stream ");
         md.stopStream();
     }
 }
